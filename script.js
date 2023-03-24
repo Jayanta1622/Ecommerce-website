12c2020a8da594afd962496051f204a0918db783
Jayanta Chatterjee <93910315+Jayanta1622@users.noreply.github.com>
2023-03-24 10:32:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const hamburger= document.querySelector('.hamburger');
+const navbar= document.querySelector('#navbar');
+hamburger.addEventListener('click',function () {
+    navbar.classList.toggle('activeNav');
+    });
+
+
 const track1 = document.getElementById("track");
 window.onmousedown = e =>{
     track1.dataset.mouseDownAt = e.clientX;
@@ -9,7 +16,8 @@ window.onmouseup = () =>{
 window.onmousemove = e =>{
     if (track1.dataset.mouseDownAt ==="0") return;
 
-    const mouseDelta= parseFloat(track1.dataset.mouseDownAt)- e.clientX ;const maxDelta = window.innerWidth/2;
+    const mouseDelta= parseFloat(track1.dataset.mouseDownAt)- e.clientX ;
+    const maxDelta = window.innerWidth/2;
 
     const percentage = (mouseDelta / maxDelta)* -100;
     const nextpercentage = Math.min(Math.max(parseFloat(track1.dataset.prevPercentage) + percentage, -100), 0);
